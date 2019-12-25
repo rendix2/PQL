21d3821069be7a45a6766b0613905a2971df2cda
Tomáš Babický <rendix2@seznam.cz>
2019-12-25 02:54:47 +0100

    Implement new union #60

--- query/Explain.php
+++ query/Explain.php
@@ -264,13 +264,13 @@ class Explain extends BaseQuery
     {
         $tables = $this->explainHelper($this->query);
 
-        foreach ($this->query->getUnionAllQueries() as $i => $unionAllQuery) {
-            $result = $this->explainHelper($unionAllQuery);
+        foreach ($this->query->getUnionQueries() as $i => $unionQuery) {
+            $result = $this->explainHelper($unionQuery);
             $tables[] = new Row(
                 [
                     'table' => '',
                     'rows' => '',
-                    'type' => 'UNION ALL #' . $i,
+                    'type' => 'UNION #' . $i,
                     'condition' => '',
                     'algorithm' => '',
                 ]
@@ -279,13 +279,13 @@ class Explain extends BaseQuery
             $tables = array_merge($tables, $result);
         }
 
-        foreach ($this->query->getUnionQueries() as $i => $unionQuery) {
-            $result = $this->explainHelper($unionQuery);
+        foreach ($this->query->getUnionAllQueries() as $i => $unionAllQuery) {
+            $result = $this->explainHelper($unionAllQuery);
             $tables[] = new Row(
                 [
                     'table' => '',
                     'rows' => '',
-                    'type' => 'UNION #' . $i,
+                    'type' => 'UNION ALL #' . $i,
                     'condition' => '',
                     'algorithm' => '',
                 ]
